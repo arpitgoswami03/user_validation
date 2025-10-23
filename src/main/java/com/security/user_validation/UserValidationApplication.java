47d1cc515ecb424841717dd6c9f5f9395a436086
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-10-24 00:46:47 +0530

    first commit

---
+++ src/main/java/com/security/user_validation/UserValidationApplication.java
@@ -0,0 +1,13 @@
+package com.security.user_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserValidationApplication.class, args);
+    }
+
+}
